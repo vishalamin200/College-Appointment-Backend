d7a64a4623ec3a64dda5d107305aa4b8461440b9
Vishal Amin <vishalamin200@gmail.com>
2025-07-31 20:12:33 +0530

    removed empty file in root  and updated package.json

--- tests/test.js
+++ tests/test.js
@@ -1,14 +1,14 @@
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import request from 'supertest';
 import app from '../app.js';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
 
 describe('E2E Appointment Flow (JWT Auth)', () => {
     let agentP, agentA1, agentA2;
     let professorId;
-    
-    beforeAll(async ()=>{
-        
+
+    beforeAll(async () => {
+
         dotenv.config()
         await mongoose.connect(process.env.MONGO_URI);
 
@@ -16,39 +16,37 @@ describe('E2E Appointment Flow (JWT Auth)', () => {
         agentA1 = request.agent(app);
         agentA2 = request.agent(app);
 
-
     })
-
 
     test('Full appointment flow', async () => {
 
         // student A1 login
         await agentA1
             .post('/api/auth/login')
-            .send({email: 'a1@test.com', password: '12345' });
+            .send({ email: 'a1@test.com', password: '12345' });
 
 
         //  professor P login
         const profRes = await agentP
             .post('/api/auth/login')
-            .send({email: 'p@test.com', password: '12345'});
+            .send({ email: 'p@test.com', password: '12345' });
 
-    
-        professorId = profRes.body.user._id; 
+
+        professorId = profRes.body.user._id;
 
 
         // Professor P sets availability T
         const slotT = {
             start: "2025-08-05T09:00:00.000Z",
-            end: "2025-08-05T12:00:00.000Z" 
+            end: "2025-08-05T12:00:00.000Z"
         };
-    
+
         await agentP.post('/api/availability').send({ slots: [slotT] });
 
 
         // Student A1 views available slots for Professor P
         const view1 = await agentA1.get(`/api/availability/${professorId}`);
-        expect(Array.isArray(view1.body.availability.slots)).toBe(true); 
+        expect(Array.isArray(view1.body.availability.slots)).toBe(true);
 
 
         // Student A1 books appointment T1
@@ -61,14 +59,14 @@ describe('E2E Appointment Flow (JWT Auth)', () => {
             .post('/api/appointments')
             .send({ professorId, slot: slotT1 });
 
-        
+
         const appointment1 = book1.body.appointment;
 
 
         // Student A2 authenticates
         await agentA2
             .post('/api/auth/login')
-            .send({email: 'a2@test.com', password: '12345'});
+            .send({ email: 'a2@test.com', password: '12345' });
 
 
         // Professor P1 sets availability T2
